7fc9adc1fb2ce1221be2674df17bee38f1e26330
Andrey <smokels1998@gmail.com>
2022-10-11 16:44:40 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import store from './redux/redux-store';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
