ab9b7835d9542dc016aa41f026ea73022d1ceaa2
Andrey <smokels1998@gmail.com>
2022-11-11 09:14:01 +0300

    some map fixes

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -7,27 +7,33 @@ import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
   const position = [51.505, -0.09];
+  const bounds = [
+    [
+      [-90, -Infinity],
+      [90, Infinity],
+    ],
+  ];
 
   const MapContainerStyle = {
     height: 'calc(100vh - 66px)',
     width: '100%',
     position: 'relative',
     top: 0,
-    left:0,
+    left: 0,
     marginTop: -20,
-  }
-  
+  };
+
   return (
     <>
       {/* @ts-ignore */}
-      <MapContainer center={position} zoom={2} scrollWheelZoom={false} style={MapContainerStyle}>
-        {/* <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/1/1/1.png"
-        /> */}
+      <MapContainer center={position} zoom={2} style={MapContainerStyle} maxBounds={bounds}
+        scrollWheelZoom={true}
+        attributionControl={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          detectRetina
+          url="https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=82cd9c64b0e678fe5ce342593e19f7de"
         />
         {/* @ts-ignore */}
         <Marker position={position}>
